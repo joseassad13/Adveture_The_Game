8eec5a518c9fbf42c76eeaf1e4af0e01c9715fc5
joseassad13 <159552383+joseassad13@users.noreply.github.com>
2024-06-10 05:23:04 -0600

    Creacion de mapa del juego

---
+++ include/Sala.h
@@ -0,0 +1,23 @@
+#include <SFML/Graphics.hpp>
+#include "Pared.h"
+#include <vector>
+
+#ifndef SALA_H
+#define SALA_H
+class Sala
+{
+private:
+   sf::Vector2i posSalida;
+   sf::Vector2i posJugador;
+   std::vector<sf::Vector2i> posEnemigo;
+   void setUpInitialState();
+   void setUpPosEnemigo();
+   void setUpParedes();
+public:
+    std::vector< std::vector<Pared *> > paredes;
+    int tamanioPared;
+    Sala();
+};
+
+
+#endif
--- src/adventure_game.cpp
+++ src/adventure_game.cpp
@@ -1,4 +1,4 @@
-#include <ftxui/dom/elements.hpp>
+/*#include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
 #include <ftxui/screen/color.hpp>
 #include <iostream>
@@ -6,6 +6,7 @@
 #include <thread>
 #include <list>
 #include <fstream>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 using namespace ftxui;
@@ -63,4 +64,37 @@ int main(int argc, char const *argv[])
     }
 
     return 0;
+}*/
+#include<SFML/Graphics.hpp>
+#include "pared.cpp"
+#include "sala.cpp"
+
+int main(){
+    float windowHeight = 400;
+    float windowWidth = 400;
+
+    sf::RenderWindow window(sf::VideoMode(windowWidth,windowHeight),"ADVENTURE");
+    
+    Sala sala = Sala();
+
+    while(window.isOpen()){
+        sf::Event event;
+
+
+        while(window.pollEvent(event)){
+            if(event.type == sf::Event::Closed){
+                window.close();
+            }
+
+            window.clear();
+
+            for (int i = 0; i < sala.tamanioPared; i++ ) {
+                for (int j = 0; j < sala.tamanioPared; j++ ) {
+                    window.draw(sala.paredes[i][j]->sprite);
+                }
+            }
+
+            window.display();
+        }
+    }
 }
