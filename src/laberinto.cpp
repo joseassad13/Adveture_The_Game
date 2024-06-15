05c3c07ee6e40db71f90649a72b1a80a3d35d222
LuisEnriqueGonzalezIsaac <160062067+LuisEnriqueGonzalezIsaac@users.noreply.github.com>
2024-06-14 18:09:05 -0600

    Update laberinto.cpp

--- src/laberinto.cpp
+++ src/laberinto.cpp
@@ -1,86 +1,80 @@
-#include "stdafx.h"
 #include <iostream>
 #include <ctime>
 
 using namespace std;
-using namespace System;
-                        //filas  columnas densidad
-void generarLaberinto (int filas, int columnas, float densidad)
+
+// filas  columnas densidad
+void generarLaberinto(int filas, int columnas, float densidad)
 {
-    //matriz doble para que sea dinamica
+    // matriz doble para que sea dinamica
     int **matriz;
     matriz - new int *[filas];
-    int FParedes = densidad*8;
-    densidad = filas*columnas*densidad/4;
+    int FParedes = densidad * 8;
+    densidad = filas * columnas * densidad / 4;
 
-    //para fila crea numero de columnas
+    // para fila crea numero de columnas
     for (int i = 0; i < filas; i++)
     {
-        matriz(i)= new int [columnas];
+        matriz[i] = new int[columnas];
     }
-    //valores
-     for (int i = 0; i < filas; i++)
+    // valores
+    for (int i = 0; i < filas; i++)
     {
         for (int j = 0; j < columnas; j++)
         {
-            //punto donde van los muros creacion de bordes
-            if (i==0 || j==0 || i == filas - 1 || j == columnas - 1)
+            // punto donde van los muros creacion de bordes
+            if (i == 0 || j == 0 || i == filas - 1 || j == columnas - 1)
             {
                 matriz[i][j] = 1;
             }
-            else 
+            else
             {
                 matriz[i][j] = 0;
             }
         }
-        
     }
-    //laberinto
+    // laberinto
     srand(time(NULL));
-    //ayuda a evitar que se escriba valores dentro de los muros
+    // ayuda a evitar que se escriba valores dentro de los muros
     for (int i = 0; i < densidad; i++)
     {
         int x = rand() % (columnas - 4) + 2;
-        x = (x/2)*2
+        x = (x / 2) * 2;
         int y = rand() % (filas - 4) + 2;
-        y = (y/2)*2
-        //se vuelve muro
+        y = (y / 2) * 2;
+        // se vuelve muro
         matriz[y][x] = 1;
-        for (int j = 0; j < FParedes; J++)
+        for (int j = 0; j < FParedes; j++)
         {
-           int mx[4] ={x, x, x+2, x-2};
-            int my[4] = {y+2, y-2, y, y};
-            int r = rand()% 4;
-            if (matriz[my[r]][mx[r]]== 0)
+            int mx[4] = {x, x, x + 2, x - 2};
+            int my[4] = {y + 2, y - 2, y, y};
+            int r = rand() % 4;
+            if (matriz[my[r]][mx[r]] == 0)
             {
-                matriz[my[r]][mx[r]]== 1;
-                matriz[my[r]+ (y - my[r])/2][mx[r]+ (x-mx[r])/2]== 1;
+                matriz[my[r]][mx[r]] == 1;
+                matriz[my[r] + (y - my[r]) / 2][mx[r] + (x - mx[r]) / 2] == 1;
             }
-            
         }
-        
-
     }
-    //pintar
+    // pintar
     for (int i = 0; i < filas; i++)
     {
         for (int j = 0; j < columnas; j++)
         {
-            if (matriz[i][j]==1)
+            if (matriz[i][j] == 1)
             {
-                cout<<"1";
+                cout << "1";
             }
-            if (matriz[i][j]==o)
+            if (matriz[i][j] == 0)
             {
-                cout <<" ";
+                cout << " ";
             }
         }
-       cout <<"\n"; 
+        cout << "\n";
     }
-    
 }
 
-int main
+int main()
 {
-    generarLaberinto(25,19,1);
+    generarLaberinto(25, 19, 1);
 }
