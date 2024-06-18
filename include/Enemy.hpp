3a9b36adcbee66eef80695efdc5ddaddc5e94e78
joseassad13 <159552383+joseassad13@users.noreply.github.com>
2024-06-18 09:42:19 -0600

    Modificacion para eliminar movimiento diagonal

--- include/Enemy.hpp
+++ include/Enemy.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <GameObject.hpp>
-
+#include <cmath> // Necesario para std::sqrt
 
 class Enemy : public GameObject
 {
@@ -13,44 +13,120 @@ public:
     Enemy(const sf::Texture &texture, const sf::Vector2f &position, float speed)
         : GameObject(texture, position, sf::Vector2f(0.3f, 0.3f)), speed(speed), alive(true)
     {
+        // Establecer el rectángulo de textura inicial del sprite
         sprite.setTextureRect(sf::IntRect(64, 64, 64, 64));
     }
 
     void updateAnimation(int frameCount, int frameWidth, int frameHeight, sf::Clock &animationClock, int &currentFrame)
     {
+        // Cambiar el marco de animación cada 0.15 segundos
         if (animationClock.getElapsedTime().asSeconds() > 0.15f)
         {
             currentFrame = (currentFrame + 1) % 2;
             sprite.setTextureRect(sf::IntRect((currentFrame * 69), 0, 69, 185));
             animationClock.restart();
         }
-        // (currentFrame * frameWidth, 45, frameWidth, frameHeight)
     }
 
     void updateMovement(const sf::Vector2f &targetPosition)
     {
         if (alive)
         {
+            // Calcular la dirección hacia el objetivo
             sf::Vector2f direction = targetPosition - sprite.getPosition();
-            float length = std::sqrt(direction.x * direction.x + direction.y * direction.y);
-            if (length != 0)
-                direction /= length;
+            
+            // Normalizar la dirección para moverse solo en una dimensión a la vez
+            if (std::abs(direction.x) > std::abs(direction.y))
+            {
+                direction.y = 0;
+                direction.x = direction.x > 0 ? 1 : -1;
+            }
+            else
+            {
+                direction.x = 0;
+                direction.y = direction.y > 0 ? 1 : -1;
+            }
+
             sprite.move(direction * speed);
         }
     }
 
     void checkRespawn()
     {
+        // Revivir al enemigo después de 3 segundos
         if (!alive && respawnClock.getElapsedTime().asSeconds() > 3.f)
         {
             alive = true;
+            // Reposicionar al enemigo en una ubicación predeterminada (100, 100)
             sprite.setPosition(100.f, 100.f);
         }
     }
 
     void kill()
     {
+        // Matar al enemigo y reiniciar el reloj de respawn
         alive = false;
         respawnClock.restart();
     }
 };
+
+
+
+
+
+// #pragma once
+// #include <SFML/Graphics.hpp>
+// #include <GameObject.hpp>
+
+
+// class Enemy : public GameObject
+// {
+// public:
+//     float speed;
+//     bool alive;
+//     sf::Clock respawnClock;
+
+//     Enemy(const sf::Texture &texture, const sf::Vector2f &position, float speed)
+//         : GameObject(texture, position, sf::Vector2f(0.3f, 0.3f)), speed(speed), alive(true)
+//     {
+//         sprite.setTextureRect(sf::IntRect(64, 64, 64, 64));
+//     }
+
+//     void updateAnimation(int frameCount, int frameWidth, int frameHeight, sf::Clock &animationClock, int &currentFrame)
+//     {
+//         if (animationClock.getElapsedTime().asSeconds() > 0.15f)
+//         {
+//             currentFrame = (currentFrame + 1) % 2;
+//             sprite.setTextureRect(sf::IntRect((currentFrame * 69), 0, 69, 185));
+//             animationClock.restart();
+//         }
+//         // (currentFrame * frameWidth, 45, frameWidth, frameHeight)
+//     }
+
+//     void updateMovement(const sf::Vector2f &targetPosition)
+//     {
+//         if (alive)
+//         {
+//             sf::Vector2f direction = targetPosition - sprite.getPosition();
+//             float length = std::sqrt(direction.x * direction.x + direction.y * direction.y);
+//             if (length != 0)
+//                 direction /= length;
+//             sprite.move(direction * speed);
+//         }
+//     }
+
+//     void checkRespawn()
+//     {
+//         if (!alive && respawnClock.getElapsedTime().asSeconds() > 3.f)
+//         {
+//             alive = true;
+//             sprite.setPosition(100.f, 100.f);
+//         }
+//     }
+
+//     void kill()
+//     {
+//         alive = false;
+//         respawnClock.restart();
+//     }
+// };
--- include/Juego.hpp
+++ include/Juego.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <Jugador.hpp>
+#include <Player.hpp>
 #include <SFML/Graphics.hpp>
 #include <math.h>
 
