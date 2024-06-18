439e65acd9a22f456200a27f8ab0a99b1de08b2a
LuisEnriqueGonzalezIsaac <160062067+LuisEnriqueGonzalezIsaac@users.noreply.github.com>
2024-06-18 13:26:03 -0600

    Update Laberinto.hpp

--- include/Laberinto.hpp
+++ include/Laberinto.hpp
@@ -14,7 +14,46 @@ private:
     list<list<sf::Sprite>> mapa1;
 
 public:
-    Laberinto(std::string filename)
+    sf::Sprite generarBloque(int tipo, sf::Texture &texture)
+    {
+        sf::Sprite bloque;
+
+        int bloquePosX;
+        int bloquePosY;
+
+        switch (tipo)
+        {
+        case 0:
+            bloquePosX = 1;
+            bloquePosY = 2;
+            break;
+        case 1:
+            bloquePosX = 4;
+            bloquePosY = 1;
+            break;
+        case 2:
+            bloquePosX = 0;
+            bloquePosY = 3;
+            break;
+
+        default:
+            break;
+        }
+
+        bloque = sf::Sprite(texture);
+
+        int bloqueTamaño = 32;
+        bloque.setTextureRect(
+            sf::IntRect(
+                bloquePosX * bloqueTamaño,
+                bloquePosY * bloqueTamaño,
+                bloqueTamaño,
+                bloqueTamaño));
+
+        return bloque;
+    }
+
+        Laberinto(std::string filename="./assets/salas/laberinto1.txt")
     {
         std::string line;
 
@@ -64,46 +103,9 @@ public:
         // Cerrar el archivo
         inputFile.close();
     }
-    sf::Sprite generarBloque(int tipo, sf::Texture &texture)
-    {
-        sf::Sprite bloque;
-
-        int bloquePosX;
-        int bloquePosY;
-
-        switch (tipo)
-        {
-        case 0:
-            bloquePosX = 1;
-            bloquePosY = 2;
-            break;
-        case 1:
-            bloquePosX = 4;
-            bloquePosY = 1;
-            break;
-        case 2:
-            bloquePosX = 0;
-            bloquePosY = 3;
-            break;
-
-        default:
-            break;
-        }
-
-        bloque = sf::Sprite(texture);
-
-        int bloqueTamaño = 32;
-        bloque.setTextureRect(
-            sf::IntRect(
-                bloquePosX * bloqueTamaño,
-                bloquePosY * bloqueTamaño,
-                bloqueTamaño,
-                bloqueTamaño));
-
-        return bloque;
-    }
     ~Laberinto() {}
-    void Draw(sf::RenderWindow& window){
+    void Draw(sf::RenderWindow &window)
+    {
         for (auto &&linea : mapa1)
         {
             for (auto &&bloque : linea)
@@ -112,4 +114,5 @@ public:
             }
         }
     }
+
 };
