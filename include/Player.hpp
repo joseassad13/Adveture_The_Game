1d940de9f16fc5ba035ecd852010efff39ce331c
LuisEnriqueGonzalezIsaac <160062067+LuisEnriqueGonzalezIsaac@users.noreply.github.com>
2024-06-18 22:19:17 -0600

    volvi a como estaba antes

--- include/Player.hpp
+++ include/Player.hpp
@@ -1,108 +1,40 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include "GameObject.hpp"
-#include <iostream>
+#include <GameObject.hpp>
 
 class Player : public GameObject
 {
 public:
-    int x=0;
-    int y=0;
     float moveSpeed;
     bool hasSword;
     bool hasKey;
     sf::Vector2f swordOffset;
 
-    static const int FILAS = 20;
-    static const int COLUMNAS = 20;
-
-    char laberinto[FILAS][COLUMNAS];
-
     Player(const sf::Texture &texture, const sf::Vector2f &position, float speed)
-        : GameObject(texture, position), moveSpeed(speed), hasSword(false), hasKey(false), swordOffset(50.f, 0.f), x(1), y(1)
-    {
-        // Inicialización del laberinto
-        char tempLaberinto[FILAS][COLUMNAS] =
-            {
-                {'1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'},
-                {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'},
-                {'1', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '1'},
-                {'1', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1', '0', '1', '0', '0', '1'},
-                {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', '1', '1', '1', '0', '0', '1'},
-                {'1', '0', '0', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '1'},
-                {'1', '0', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '0', '1'},
-                {'1', '0', '0', '0', '1', '0', '1', '0', '0', '0', '1', '0', '1', '0', '0', '1', '0', '0', '0', '1'},
-                {'1', '0', '1', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '0', '0', '0', '0', '0', '0', '1'},
-                {'1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '0', '0', '1'},
-                {'1', '0', '1', '0', '0', '0', '0', '0', '1', '1', '1', '1', '1', '1', '1', '0', '1', '0', '0', '1'},
-                {'1', '0', '1', '0', '1', '0', '0', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '0', '1'},
-                {'1', '0', '0', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '0', '0', '1', '0', '0', '0', '1'},
-                {'1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '0', '0', '1'},
-                {'1', '0', '1', '1', '1', '1', '1', '1', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '1'},
-                {'1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '1'},
-                {'1', '0', '0', '0', '0', '1', '1', '1', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '0', '1'},
-                {'1', '0', '0', '0', '0', '1', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '1'},
-                {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'},
-                {'1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'}};
-        std::copy(&tempLaberinto[0][0], &tempLaberinto[0][0] + FILAS * COLUMNAS, &laberinto[0][0]);
-    }
-
-    void moverPersonaje(char direccion)
-    {
-        int nuevaX = x;
-        int nuevaY = y;
-
-        if (direccion == sf::Keyboard::Left) nuevaX -= 1;
-        else if (direccion == sf::Keyboard::Right) nuevaX += 1;
-        else if (direccion == sf::Keyboard::Up) nuevaY -= 1;
-        else if (direccion == sf::Keyboard::Down) nuevaY += 1;
-
-        if (esMovimientoValido(nuevaX, nuevaY))
-        {
-            x = nuevaX;
-            y = nuevaY;
-            sprite.setPosition(x * sprite.getTexture()->getSize().x, y * sprite.getTexture()->getSize().y);
-        }
-        else
-        {
-            std::cout << "Movimiento no válido!\n";
-        }
-    }
-
-    bool esMovimientoValido(int nuevaX, int nuevaY)
-    {
-        if (nuevaX < 0 || nuevaX >= FILAS || nuevaY < 0 || nuevaY >= COLUMNAS)
-        {
-            return false; // Fuera de los límites del laberinto
-        }
-        if (laberinto[nuevaX][nuevaY] == '1')
-        {
-            return false; // Es una pared
-        }
-        return true;
-    }
+        : GameObject(texture, position), moveSpeed(speed), hasSword(false), hasKey(false), swordOffset(50.f, 0.f) {}
 
     void update()
     {
         bool moved = false;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && !moved)
+        
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && sprite.getPosition().x >= 1)
         {
-            moverPersonaje(sf::Keyboard::Left);
+            sprite.move(-moveSpeed, 0.f);
             moved = true;
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && !moved)
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && !moved && sprite.getPosition().x < 640 - sprite.getTexture()->getSize().x)
         {
-            moverPersonaje(sf::Keyboard::Right);
+            sprite.move(moveSpeed, 0.f);
             moved = true;
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && !moved)
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && !moved && sprite.getPosition().y >= 1)
         {
-            moverPersonaje(sf::Keyboard::Up);
+            sprite.move(0.f, -moveSpeed);
             moved = true;
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && !moved)
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && !moved && sprite.getPosition().y < 640 - sprite.getTexture()->getSize().y)
         {
-            moverPersonaje(sf::Keyboard::Down);
+            sprite.move(0.f, moveSpeed);
             moved = true;
         }
     }
@@ -112,111 +44,3 @@ public:
         return sprite.getGlobalBounds();
     }
 };
-// class Player : public GameObject
-// {
-// public:
-//     int x;
-//     int y;
-//     float moveSpeed;
-//     bool hasSword;
-//     bool hasKey;
-//     sf::Vector2f swordOffset;
-
-//     static const int FILAS = 20;
-//     static const int COLUMNAS = 20;
-
-//     char laberinto[FILAS][COLUMNAS];
-
-//     Player(const sf::Texture &texture, const sf::Vector2f &position, float speed)
-//         : GameObject(texture, position), moveSpeed(speed), hasSword(false), hasKey(false), swordOffset(50.f, 0.f), x(0), y(0)
-//     {
-
-//         // Inicialización del laberinto
-//         char tempLaberinto[FILAS][COLUMNAS] =
-//             {
-//                 {'1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'},
-//                 {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'},
-//                 {'1', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '1'},
-//                 {'1', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1', '0', '1', '0', '0', '1'},
-//                 {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', '1', '1', '1', '0', '0', '1'},
-//                 {'1', '0', '0', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '1'},
-//                 {'1', '0', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '0', '1'},
-//                 {'1', '0', '0', '0', '1', '0', '1', '0', '0', '0', '1', '0', '1', '0', '0', '1', '0', '0', '0', '1'},
-//                 {'1', '0', '1', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '0', '0', '0', '0', '0', '0', '1'},
-//                 {'1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '0', '0', '1'},
-//                 {'1', '0', '1', '0', '0', '0', '0', '0', '1', '1', '1', '1', '1', '1', '1', '0', '1', '0', '0', '1'},
-//                 {'1', '0', '1', '0', '1', '0', '0', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '0', '1'},
-//                 {'1', '0', '0', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '0', '0', '1', '0', '0', '0', '1'},
-//                 {'1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '0', '0', '1'},
-//                 {'1', '0', '1', '1', '1', '1', '1', '1', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '1'},
-//                 {'1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '1'},
-//                 {'1', '0', '0', '0', '0', '1', '1', '1', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '0', '1'},
-//                 {'1', '0', '0', '0', '0', '1', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '1'},
-//                 {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'},
-//                 {'1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'}};
-//         std::copy(&tempLaberinto[0][0], &tempLaberinto[0][0] + FILAS * COLUMNAS, &laberinto[0][0]);
-//     }
-
-//     void moverPersonaje(char direccion)
-//     {
-//         int nuevaX = x;
-//         int nuevaY = y;
-//         if (esMovimientoValido(nuevaX, nuevaY))
-//         {
-//             x = nuevaX;
-//             y = nuevaY;
-//         }
-//         else
-//         {
-//             std::cout << "Movimiento no válido!\n";
-//         }
-//     }
-
-//     bool esMovimientoValido(int nuevaX, int nuevaY)
-//     {
-//         if (nuevaX < 0 || nuevaX >= FILAS || nuevaY < 0 || nuevaY >= COLUMNAS)
-//         {
-//             return false; // Fuera de los límites del laberinto
-//         }
-//         if (laberinto[nuevaX][nuevaY] == '1')
-//         {
-//             return false; // Es una pared
-//         }
-//         return true;
-//     }
-//     void update()
-//     {
-
-//         bool moved = false;
-
-//         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && sprite.getPosition().x >= 1)
-//         {
-//             sprite.move(-moveSpeed, 0.f);
-//             x -= moveSpeed;
-//             moved = true;
-//         }
-//         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && !moved && sprite.getPosition().x < 640 - sprite.getTexture()->getSize().x)
-//         {
-//             sprite.move(moveSpeed, 0.f);
-//             x += moveSpeed;
-//             moved = true;
-//         }
-//         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && !moved && sprite.getPosition().y >= 1)
-//         {
-//             sprite.move(0.f, -moveSpeed);
-//             y -= moveSpeed;
-//             moved = true;
-//         }
-//         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && !moved && sprite.getPosition().y < 640 - sprite.getTexture()->getSize().y)
-//         {
-//             sprite.move(0.f, moveSpeed);
-//             y += moveSpeed;
-//             moved = true;
-//         }
-//     }
-
-//     sf::FloatRect getBoundingBox() const
-//     {
-//         return sprite.getGlobalBounds();
-//     }
-// };
--- src/laberintoPrueba.cpp
+++ src/laberintoPrueba.cpp
@@ -5,73 +5,96 @@
 
 using namespace std;
 const int FILAS = 20;
-const int COLUMNAS = 5;
+const int COLUMNAS = 20;
 
 char laberinto[FILAS][COLUMNAS] = {
-    {'1', '1', '1', '1', '1'},
-    {'1', '0', '0', '1', '1'},
-    {'1', '0', '1', '0', '1'},
-    {'1', '0', '0', '0', '1'},
-    {'1', '1', '0', '1', '1'},
-    {'1', '0', '0', '0', '1'},
-    {'1', '0', '0', '0', '1'},
-    {'1', '0', '0', '0', '1'},
-    {'1', '0', '0', '0', '1'},
-    {'1', '0', '0', '0', '1'},
-    {'1', '0', '0', '0', '1'},
-    {'1', '0', '0', '0', '1'},
-    {'1', '0', '0', '0', '1'},
-    {'1', '0', '0', '0', '1'},
-    {'1', '0', '0', '0', '1'},
-    {'1', '0', '0', '0', '1'},
-    {'1', '0', '0', '0', '1'},
-    {'1', '0', '0', '0', '1'},
-    {'1', '0', '0', '0', '1'},
-    {'1', '0', '0', '0', '1'},
-};
+    {'1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'},
+    {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'},
+    {'1', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '1'},
+    {'1', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1', '0', '1', '0', '0', '1'},
+    {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', '1', '1', '1', '0', '0', '1'},
+    {'1', '0', '0', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '1'},
+    {'1', '0', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '0', '1'},
+    {'1', '0', '0', '0', '1', '0', '1', '0', '0', '0', '1', '0', '1', '0', '0', '1', '0', '0', '0', '1'},
+    {'1', '0', '1', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '0', '0', '0', '0', '0', '0', '1'},
+    {'1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '0', '0', '1'},
+    {'1', '0', '1', '0', '0', '0', '0', '0', '1', '1', '1', '1', '1', '1', '1', '0', '1', '0', '0', '1'},
+    {'1', '0', '1', '0', '1', '0', '0', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '0', '1'},
+    {'1', '0', '0', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '0', '0', '1', '0', '0', '0', '1'},
+    {'1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '0', '0', '1'},
+    {'1', '0', '1', '1', '1', '1', '1', '1', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '1'},
+    {'1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '1'},
+    {'1', '0', '0', '0', '0', '1', '1', '1', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '0', '1'},
+    {'1', '0', '0', '0', '0', '1', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '1'},
+    {'1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'},
+    {'1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'}
+    };
 
-struct Personaje {
+struct Personaje
+{
     int x;
     int y;
 };
 
 Personaje personaje = {1, 1}; // Posición inicial del personaje
 
-bool esMovimientoValido(int nuevaX, int nuevaY) {
-    if (nuevaX < 0 || nuevaX >= FILAS || nuevaY < 0 || nuevaY >= COLUMNAS) {
+bool esMovimientoValido(int nuevaX, int nuevaY)
+{
+    if (nuevaX < 0 || nuevaX >= FILAS || nuevaY < 0 || nuevaY >= COLUMNAS)
+    {
         return false; // Fuera de los límites del laberinto
     }
-    if (laberinto[nuevaX][nuevaY] == '1') {
+    if (laberinto[nuevaX][nuevaY] == '1')
+    {
         return false; // Es una pared
     }
     return true;
 }
 
-void moverPersonaje(char direccion) {
+void moverPersonaje(char direccion)
+{
     int nuevaX = personaje.x;
     int nuevaY = personaje.y;
 
-    switch (direccion) {
-        case 'w': nuevaX--; break; // Mover arriba
-        case 's': nuevaX++; break; // Mover abajo
-        case 'a': nuevaY--; break; // Mover izquierda
-        case 'd': nuevaY++; break; // Mover derecha
+    switch (direccion)
+    {
+    case 'w':
+        nuevaX--;
+        break; // Mover arriba
+    case 's':
+        nuevaX++;
+        break; // Mover abajo
+    case 'a':
+        nuevaY--;
+        break; // Mover izquierda
+    case 'd':
+        nuevaY++;
+        break; // Mover derecha
     }
 
-    if (esMovimientoValido(nuevaX, nuevaY)) {
+    if (esMovimientoValido(nuevaX, nuevaY))
+    {
         personaje.x = nuevaX;
         personaje.y = nuevaY;
-    } else {
-        std:: cout<< "Movimiento no válido!\n";
+    }
+    else
+    {
+        std::cout << "Movimiento no válido!\n";
     }
 }
 
-void mostrarLaberinto() {
-    for (int i = 0; i < FILAS; i++) {
-        for (int j = 0; j < COLUMNAS; j++) {
-            if (i == personaje.x && j == personaje.y) {
+void mostrarLaberinto()
+{
+    for (int i = 0; i < FILAS; i++)
+    {
+        for (int j = 0; j < COLUMNAS; j++)
+        {
+            if (i == personaje.x && j == personaje.y)
+            {
                 std::cout << 'P'; // Representar al personaje
-            } else {
+            }
+            else
+            {
                 std::cout << laberinto[i][j];
             }
         }
@@ -79,14 +102,17 @@ void mostrarLaberinto() {
     }
 }
 
-int main() {
+int main()
+{
     char input;
-    while (true) {
+    while (true)
+    {
         mostrarLaberinto();
         std::cout << "Mueve al personaje (w: arriba, s: abajo, a: izquierda, d: derecha, q: salir): ";
         std::cin >> input;
 
-        if (input == 'q') break;
+        if (input == 'q')
+            break;
 
         moverPersonaje(input);
     }
